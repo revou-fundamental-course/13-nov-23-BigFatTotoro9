31f65b8449df44287b5db378bd6952c3559ee7da
BigFatTotoro9 <fadhilahrasyid9@gmail.com>
2023-11-22 19:58:58 +0700

    add auto slider image

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-const submitButton = document.getElementsByClassName("primary-button");
-// jangan pake class ntar kudu kasih urutan array
+const submitButton = document.getElementsByClassName("primary-button"); // jangan pake class ntar kudu kasih urutan array
 const nameInput = document.getElementById("name-input");
 const emailInput = document.getElementById("email-input");
 const nameOutput = document.getElementById("username");
+const imageSlider = document.getElementById("slider");
 
 submitButton[0].addEventListener("click", function (event) {
   if (nameInput.value.length === 0 || emailInput.value.length === 0) {
@@ -16,3 +16,19 @@ submitButton[0].addEventListener("click", function (event) {
 // nameInput.addEventListener("input", function (event) {
 //   console.log(event);
 // });
+
+// bikin list image
+const images = ["/assets/bg.jpg", "/assets/bg2.jpg", "/assets/bg3.jpg"];
+
+// fungsi penambahan i dan autoslider gambar
+var i = 0;
+function bannerSlide() {
+  i += 1;
+  if (i == images.length) {
+    i = 0;
+  }
+  imageSlider.src = images[i];
+}
+
+// otomasi run funsi bannerSlide
+setInterval(bannerSlide, 1000);
